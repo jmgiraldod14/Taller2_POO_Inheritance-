d2fe94c15a8b01e9b0bb43c15c30c50249aefe01
jmgiraldod14 <jmgiraldod@gmail.com>
2026-09-03 23:40:16 -0500

    Abstrac class

--- Backend/Class1.cs
+++ Backend/Class1.cs
@@ -1,6 +1,22 @@
 ﻿namespace Backend;
 
-public class Class1
+public abstract class GeometricFigure
 {
+    // Constructors
+    protected GeometricFigure(string figure)
+    {
+        Name = figure;
+    }
 
+    // Properties
+    public string Name { get; set; }
+
+    // Public Methods
+    public abstract double GetArea();
+    public abstract double GetPerimeter();
+
+    public override string ToString()
+    {
+        return $"{Name,-15} => Area.....: {GetArea(),12:N5}      Perimeter: {GetPerimeter(),12:F5}";
+    }
 }
