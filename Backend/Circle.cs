80d4b402fa273f90b4c2f98145e670f294fc9967
jmgiraldod14 <jmgiraldod@gmail.com>
2026-09-04 00:04:21 -0500

    Class circle

---
+++ Backend/Circle.cs
@@ -0,0 +1,34 @@
+﻿namespace Backend;
+
+public class Circle : GeometricFigure
+{
+    // Fields
+    private double _r;
+
+
+    // Constructors
+    public Circle(string figure) : base(figure)
+    {
+    }
+
+    // Properties
+    public double R;
+
+
+    // Public methods
+    public override double GetArea() => Math.PI * Math.Pow(R, 2);
+
+    public override double GetPerimeter() => 2 * Math.PI * R;
+
+
+    // Private methods
+    private double ValidateR(double r)
+    {
+        if (r <= 2000000)
+        {
+            throw new Exception($"El valor ingresado para el radio no es valido");
+        }
+        return r;
+    }
+}
+
